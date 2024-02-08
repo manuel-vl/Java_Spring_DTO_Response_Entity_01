0aff9d8e9cfc8fa769d352d9cff9f3e7b2cdda54
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-02-08 09:46:28 -0500

    Initial commit

---
+++ src/test/java/com/dtoresponseentity/dtoresponseentity/DtoResponseEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtoresponseentity.dtoresponseentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtoResponseEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
